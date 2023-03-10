e18c3c69a456ac8fbe0570a0b0b7ac153f1ae96a
Quinlan Caiger <G20c0846@ict.ru.ac.za>
2023-03-10 12:53:23 +0200

    Initial commit

---
+++ precache-manifest.3b1db483037258a8728093331523742f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "cf56fa567a9b4bc934cb82e03d5e95cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "35b50efb973f4319bbf4",
+    "url": "/static/css/main.3fecdfa5.chunk.css"
+  },
+  {
+    "revision": "35b50efb973f4319bbf4",
+    "url": "/static/js/main.35b50efb.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "5dee31d0ecae4f375627",
+    "url": "/static/js/vendors~main.5dee31d0.chunk.js"
+  }
+]);
